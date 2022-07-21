0147721901969cfe51ca168b75f5c362c1b5a695
wladbelsky <wladbelsky>
2022-07-21 15:45:27 +0300

    fixed output

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from typing import Any, List, Tuple
+from typing import Any, List
 from flask import Flask
 from database import Database, Client
 
 app = Flask(__name__)
+
 
 class Metric:
     def __init__(self, name: str, value: Any, tags: dict):
@@ -11,19 +12,23 @@ class Metric:
         self.tags = tags
 
     def __str__(self):
-        return f"{self.name}{{{str(self.tags)}}} {self.value}"
+        tags = ', '.join([f'{k}="{v}"' for k, v in self.tags.items()])
+        if tags:
+            tags = f'{{{tags}}}'
+        return f"{self.name}{tags} {self.value}"
 
     @classmethod
     def array_to_str(cls, array: List['Metric']):
-        return "<br>".join([str(metric) for metric in array])
+        return "\n".join([str(metric) for metric in array])
 
 
 @app.route('/metrics')
 def main():
     with Database().get_session() as session:
         count = session.query(Client).count()
-        test_metric = Metric('test_metric', count, {'client': count})
-        test_metric2 = Metric('test_metric2', 7, {'test': 777})
+        test_metric = Metric('test_metric', count, {'test': 1})
+        test_metric2 = Metric('test_metric', 7, {'test': 2})
         return Metric.array_to_str([test_metric, test_metric2])
+
 
 app.run(host='0.0.0.0', port=5000)
